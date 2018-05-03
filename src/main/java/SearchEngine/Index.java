dba318b71114bbeec2a063d1e90bcfa0c6f5b6e2
juliet24440 <juliet24440@gmail.com>
2018-05-03 18:12:15 -0400

    Added comment

--- src/main/java/SearchEngine/Index.java
+++ src/main/java/SearchEngine/Index.java
@@ -1,7 +1,27 @@
 package SearchEngine;
 
 public class Index {
+	 char[] text;
+	 char[] pattern;
+	 int foo;
+	 int java;
 	
+	public void setString(String text, String pattern) {
+		this.text = text.toCharArray();
+		this.pattern = pattern.toCharArray();
+		this.foo = text.length();
+		this.java = pattern.length();
+	}
 	
-
+	public int search() {
+		for (int i = 0; i < foo-java; i++) {
+			int j = 0;
+			while (j < java && text[i+j] == pattern[j]) {
+				j++;
+			}
+			if (j == java) return i;
+		}
+		return -1;
+	}
 }
+//This is an Index for the Search Engine project 
